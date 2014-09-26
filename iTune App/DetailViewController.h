adbdd08bc0157b753d5f210c33d319f2911d57d8
synerzip <synerzip@imac02.synerzipune.local>
2014-09-26 14:35:35 +0530

    Initial Commit

---
+++ iTune App/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  iTune App
+//
+//  Created by synerzip on 26/09/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
---
+++ iTune App/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  iTune App
+//
+//  Created by synerzip on 26/09/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
